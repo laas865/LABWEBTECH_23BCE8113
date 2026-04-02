9ea56e5c5ebde185870ad294badea8331076bb30
laas865 <laasya865@gmail.com>
2026-04-02 22:49:50 +0530

    Create App.js

---
+++ LAB10/App.js
@@ -0,0 +1,22 @@
+import FormValidation from "./FormValidation";
+import ItemList from "./ItemList";
+import FetchData from "./FetchData";
+function App() {
+  return (
+    <div>
+      {/* ================= LAB 10 ================= */}
+      <h2>Lab Sheet 10</h2>
+
+      <h3>Exercise 1: Form Validation</h3>
+      <FormValidation />
+
+      <h3>Exercise 2: Dynamic Item List</h3>
+      <ItemList />
+
+      <h3>Exercise 3: Fetch Data from API</h3>
+      <FetchData />
+    </div>
+  );
+}
+
+export default App;
